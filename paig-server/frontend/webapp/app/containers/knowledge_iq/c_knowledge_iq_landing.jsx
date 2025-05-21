3423d6792ecb347268c645bd4fb17d1533cb55c0
Ketan Hirani <hiraniketan@gmail.com>
2025-05-21 14:28:01 +0530

    added missing file

---
+++ paig-server/frontend/webapp/app/containers/knowledge_iq/c_knowledge_iq_landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import BaseContainer from 'containers/base_container';
+
+class CKnowledgeIQLanding extends Component {
+    render() {
+        return (
+            <BaseContainer>
+                <div>Knowledge IQ</div>
+            </BaseContainer>
+        )
+    }
+}
+
+export default CKnowledgeIQLanding;
